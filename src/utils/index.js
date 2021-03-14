c9011d9975575dfb9c2df92c195329e4197a5c5d
Olagammal <osolagammal@gmail.com>
2021-03-14 11:35:11 +0530

    removed utils folder

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { logger } from "redux-logger";
-import rootReducer from "../reducer";
-
-export const testStore = (initialState) => {
-  const storeWithMiddleware = applyMiddleware(logger)(createStore);
-  return storeWithMiddleware(rootReducer, initialState);
-};
